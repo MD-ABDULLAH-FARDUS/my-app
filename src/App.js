3e7598699c41e55f53bfaa0707f66e8e577d4a09
md-abdullah-fardus <abdullahferdus@gmail.com>
2021-04-19 19:58:00 +0600

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
-
-
+const labelStyle = {
+  display: "inline-block",
+  fontSize: "1.5rem",
+  marginRight: "20px",
+  marginBottom: "10px"
+}
+const inputStyle = {
+  width: "400px",
+  height: "30px",
+  textTransform: "uppercase"
+}
+const buttonStyle = {
+  width: "200px",
+  height: "60px",
+  fontSize: "2rem"
+}
+const man = () => {
+  let name = document.getElementById("name").value 
+  let email = document.getElementById("email").value
+  alert (`User Name is : ${name} and User mail is : ${email}`)
+}
 function App() {
-  return (
-    <div>
-      <h1>My App</h1>
-    </div>
-  );
+    return (
+        <div>
+          <div>
+           <label style={labelStyle} htmlFor="name">Name:</label>
+           <input type="text" style={inputStyle} id="name"/>
+          </div>
+          <div>
+           <label style={labelStyle} htmlFor="email">Email:</label>
+           <input type="email" name="" id="email" style={inputStyle}/>
+          </div>
+          <button style={buttonStyle} onClick={man}>Submit</button>
+        </div>
+    );
 }
 
 export default App;
